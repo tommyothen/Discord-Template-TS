9ac6ae41e4c66db44d0aa07e19e6c074efbaf09d
DaSushiAsian <42751764+DaSushiAsian@users.noreply.github.com>
2021-02-17 12:52:04 +0000

    Added message handling

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,23 @@ client.on("ready", () => {
 client.on("message", async (msg) => {
   if (msg.author.bot) return;
   if (msg.channel.type === "dm") return;
+
+  // Set the prefix
+  const prefix = "!";
+
+  // Format message and args
+  const messageArray = msg.content.split(" ");
+  const cmd = messageArray[0];
+  const strippedCmd = cmd.replace(prefix, "")
+  const args = messageArray.splice(1);
+
+  // Grab command if exists
+  const commandFile = client.commands.get(strippedCmd) ||
+    client.commands.get(client.aliases.get(strippedCmd));
+
+  if (commandFile && cmd.startsWith(prefix)) {
+    commandFile.run({client, msg, args});
+  }
 });
 
 // Handler for error event
